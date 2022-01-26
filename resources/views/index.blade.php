d67c29b72d6d55ffe6cd8fc59210045c40fe28a8
bnibo9315 <longthanh@hekategroup.com>
2022-01-26 09:58:00 +0700

    change layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,66 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
- <meta charset="UTF-8">
- <meta http-equiv="X-UA-Compatible" content="IE=edge">
- <meta name="viewport" content="width=device-width, initial-scale=1.0">
- <title>Bot Chats - Việt Nam</title>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha512-SfTiTlX6kk+qitfevl/7LibUOeJWlt9rbyDn92a1DqWOw9vWG2MFoays0sgObmWazO5BQPiFucnnEAjpAB+/Sw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
- <link rel="stylesheet" href="{{ URL::asset('./css/app.css'); }} ">
- 
-</head>
+<?php
+include_once('../layouts/header.php');
+?>
+
 <body>
- <header class="header">
-  <nav class="top-bar">
-   <div class="container-fluid">
-    <div class="row">
-     <div class="wrapper">
-      <ul class="nav-list">
-       <li class="nav-item"><a href="">Về chúng tôi</a></li>
-       <li class="nav-item"><a href="">Chat - Bot</a></li>
-       <li class="nav-item"><a href="">Crypto</a></li>
-       <li class="nav-item"><a href="">Blog</a></li>
-      </ul>
-      <div class="form-login">
-       <button class="btn btn-signup">Đăng ký</button>
-       <button class="btn btn-login">Đăng nhập</button>
-      </div>
-     </div>
-    </div>
-   </div>
-  </nav>
- </header>
- <main class="main">
-  <section class="started">
-   <div class="container-fluid">
-    <div class="row">
-     <div class="wrapper">
-      <div class="started-left">
-       <img class="image" src="./data/img/bg-index.png" alt="">
-      </div>
-      <div class="started-right">
-       <div class="wrap">
+  <main class="main">
+    <section class="intro">
+      <div class="box-intro">
         <div class="box-title">
-         <h3 class="title">Tạo chat-bot phù hợp với nhiều nền tảng
-         </h3>
+          <h2 class="title" id="title">Nền tảng xây dựng Chatbot thông minh</h2>
+
         </div>
-        <p class="description">ChatBots - một công cụ giúp bạn giải quyết các công việc trên nhiều nền tảng như Facebook, Zalo.
-         </p>
-         <button class="btn btn-started">Bắt đầu nào !!!</button>
-       </div>
+        <p class="desc">Tạo và triển khai trợ lý ảo thông minh hỗ trợ dịch vụ khách hàng và kết quả bán hàng của bạn bằng cách mang lại một cấp độ tự động hóa, tốc độ và tính khả dụng mới.</p>
+        <div class="box-img">
+          <img src="../data/img/img2.png" alt="REALBOT" title="REALBOT" class="img">
+        </div>
       </div>
-      </div>
-     </div>
-   </div>
-  </section>
- </main>
- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script> 
- <script src="{{ URL::asset('./js/app.js'); }}"></script>
- 
+    </section>
+  </main>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+  <script src="{{ URL::asset('../js/app.css'); }}"></script>
 </body>
+
 </html>
