4b69a2e7e96e02911b4cdce43bbc02241ddc3422
bnibo9315 <longthanh@hekategroup.com>
2022-01-26 10:28:53 +0700

    fix bug

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,4 @@
-<?php
-include_once('../layouts/header.php');
-?>
+@include('layouts/header');
 
 <body>
   <main class="main">
@@ -12,14 +10,14 @@ include_once('../layouts/header.php');
         </div>
         <p class="desc">Tạo và triển khai trợ lý ảo thông minh hỗ trợ dịch vụ khách hàng và kết quả bán hàng của bạn bằng cách mang lại một cấp độ tự động hóa, tốc độ và tính khả dụng mới.</p>
         <div class="box-img">
-          <img src="../data/img/img2.png" alt="REALBOT" title="REALBOT" class="img">
+          <img src="./data/img/img2.png" alt="REALBOT" title="REALBOT" class="img">
         </div>
       </div>
     </section>
   </main>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-  <script src="{{ URL::asset('../js/app.css'); }}"></script>
+  <script src="{{ URL::asset('./js/app.js'); }}"></script>
 </body>
 
 </html>
